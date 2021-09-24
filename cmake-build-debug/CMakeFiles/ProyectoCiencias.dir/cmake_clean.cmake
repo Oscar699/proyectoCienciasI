9df49df48cb99ba43182b9d0dc764f15b6333a8c
Oscar699 <52666582+Oscar699@users.noreply.github.com>
2021-09-24 18:48:39 -0500

    Primerisimo commit del proyecto

---
+++ cmake-build-debug/CMakeFiles/ProyectoCiencias.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ProyectoCiencias.dir/main.cpp.obj"
+  "ProyectoCiencias.exe"
+  "ProyectoCiencias.exe.manifest"
+  "ProyectoCiencias.pdb"
+  "libProyectoCiencias.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ProyectoCiencias.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
